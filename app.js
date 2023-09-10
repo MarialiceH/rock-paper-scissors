1337f0eb2d7df5b7d609adf1a1c78ed17dd051a0
MarialiceH <137061846+MarialiceH@users.noreply.github.com>
2023-09-10 08:04:18 -0400

    Updated funcitons

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const computerChoice = document.getElementById('computer-choice');
+const computerChoiceDisplay = document.getElementById('computer-choice');
 const userChoiceDisplay = document.getElementById('user-choice');
 const resultDsiplay = document.getElementById('result');
 const possibleChoices = document.querySelectorAll('button');
 
 let userChoice
+let computerChoice
 
 possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => {
     userChoice = e.target.id
@@ -12,5 +13,17 @@ possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('cli
 }))
 
 function generateComputerChoice() {
-    const randomNumber = Math.random()
+    const randomNumber = Math.floor(Math.random() * 3) + 1
+    console.log(randomNumber)
+
+    if (randomNumber === 1) {
+        computerChoice = 'rock'
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'scissors'
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'paper'
+    }
+    computerChoiceDisplay.inner
 }
